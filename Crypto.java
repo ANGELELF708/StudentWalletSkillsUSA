0a66dfe732ab08538edf08fc0e495f05f623e5fb
ANGELELF708 <124015093+ANGELELF708@users.noreply.github.com>
2023-02-09 16:34:50 -0800

    Update Crypto.java

--- Crypto.java
+++ Crypto.java
@@ -1,18 +1,18 @@
-public class Crypto {
+public class Crypto {                                                           //object class for saving crypto wallet information
 //variables
-  private String name;
-  private String passPhrase;
-  private String walletAddress;
-  private String nl = System.getProperty("line.separator");
-  private String seperator = "---------------------------------------------";
+  private String name;                                                          //saves the user's wallet username to a string
+  private String passPhrase;                                                    //saves the user's wallet passphrase to a string
+  private String walletAddress;                                                 //saves the user's wallet address to a stirng
+  private String nl = System.getProperty("line.separator");                     //creates a new line for toString method
+  private String seperator = "---------------------------------------------";   //line seperator for readablity when printing the toString method
 //constructors
-  public Crypto(String name, String passPhrase, String walletAddress) {
+  public Crypto(String name, String passPhrase, String walletAddress) {         //constructors
     this.name = name;
     this.passPhrase = passPhrase;  
     this.walletAddress = walletAddress;
   }
 //methods
-  public String getCryptoInfo() {
-    return name + nl + walletAddress + nl + passPhrase + nl + seperator;
+   public String getCryptoInfo() {                                              //toString method 
+    return name + nl + walletAddress + nl + passPhrase + nl + seperator; 
   }
 } 
