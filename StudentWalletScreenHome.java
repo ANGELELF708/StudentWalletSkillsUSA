2edb34961a6700aee9caa0603c2747b80d0468f0
ANGELELF708 <124015093+ANGELELF708@users.noreply.github.com>
2023-02-09 19:13:06 -0800

    Update StudentWalletScreenHome.java

--- StudentWalletScreenHome.java
+++ StudentWalletScreenHome.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import java.util.*;
-public class StudentWalletScreenHome {
-  public static void print() {
+public class StudentWalletScreenHome { //This is the homescreen for the program 
+  public static void print() { //prints the following:
 //Title
-    NewScreen reset = new NewScreen("Student Wallet", "Home");
-    reset.newScreen();
-    System.out.println("Welcome " + Login.username + "!");    
+    NewScreen reset = new NewScreen("Student Wallet", "Home"); //creates and object to format the Screen with the header "Student Wallet" and the subheader "Home"
+    reset.newScreen(); //clears the screen and formats as mentioned above (see line: 6)
+    System.out.println("Welcome " + Login.username + "!");    //Prints a welcome message using the username
 //Menu Options
-    System.out.println("(0) Log Out");
-    System.out.println("(1) ID");
-    System.out.println("(2) Credit/Debit");
+    System.out.println("(0) Log Out"); //prints insturction for log out menu option
+    System.out.println("(1) ID"); //prints instruction to access the ids
+    System.out.println("(2) Credit/Debit"); //print instruciton to access the credit/debit card options
     System.out.println("(3) Crypto");  
     Scanner reader = new Scanner(System.in);
     String menuOptions = reader.next();
