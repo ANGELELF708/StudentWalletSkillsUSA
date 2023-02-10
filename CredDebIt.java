9abd6d4c3791b2e6fa0d0ba97ee4991b4db615d3
ANGELELF708 <124015093+ANGELELF708@users.noreply.github.com>
2023-02-09 16:27:22 -0800

    Update CredDebIt.java

--- CredDebIt.java
+++ CredDebIt.java
@@ -1,32 +1,33 @@
-public class CredDebIt { //Object class for credit/debit card information
+public class CredDebIt { 													//Object class for credit/debit card information
 //variables
-  private boolean credit; //"is this a credit card?" boolean
-  private String cardNum; //string for storing the card number
-  private String expDate; //string for storing the experation date
-  private String secCode; //string for storing the security code
-  private String nameFirst; //String for storing user's first name
-  private String nameLast; //string for storing user's last name
-  private String seperator = "---------------------------------------------"; //this is a seperator for readibility when printing (probs would've been worth to just make this a class)
-  private String nl = System.getProperty("line.separator");
-  private String creditOrDebit;
+  private boolean credit; 													//"is this a credit card?" boolean
+  private String cardNum; 													//string for storing the card number
+  private String expDate; 													//string for storing the experation date
+  private String secCode; 													//string for storing the security code
+  private String nameFirst; 													//String for storing user's first name
+  private String nameLast; 													//string for storing user's last name
+  private String seperator = "---------------------------------------------"; 							//this is a seperator for readibility when printing (probs would've been worth to just make this a class)
+  private String nl = System.getProperty("line.separator"); 									//this is a line spacer for the toString method "getCredDebIt"
+  private String creditOrDebit; 												//This is for printing the "credit" boolean int he toString mehtod "getCredDebIT
 //constructors 
-  public CredDebIt(boolean credit, String cardNum, String expDate, String secCode, String nameFirst, String nameLast) {
-    this.credit = credit;
-    this.cardNum = cardNum;
+  public CredDebIt(boolean credit, String cardNum, String expDate, String secCode, String nameFirst, String nameLast) { 	//constructors
+    this.credit = credit; 
+    this.cardNum = cardNum; 
     this.expDate = expDate;
     this.secCode = secCode;
     this.nameFirst = nameFirst;
     this.nameLast = nameLast;
-    if (credit == true) {
-      creditOrDebit = "Credit Card:";
+    if (credit == true) { 													//sets the stirng for toString variable "creditOrDebit" based on the boolean "credit"
+      creditOrDebit = "Credit Card:"; 												//sets the string to "Credit Card" if its a credit card
     }
     else {
-     creditOrDebit = "Debit Card:";
+     creditOrDebit = "Debit Card:"; 												//sets the string to "Debit Card" if its not a credit card (therefore must be a debit card)
     }
   }
 
 //methods
-  public boolean isCredit() {
+	//REPLIT GENERATED GET AND RETURN METHODS
+  public boolean isCredit() { 
 	 return credit;
   }
   public void setCredit(boolean credit) {
@@ -62,7 +63,7 @@ public class CredDebIt { //Object class for credit/debit card information
   public void setNameLast(String nameLast) {
 	 this.nameLast = nameLast;
   }  
-  public String getCredDebIt() {
+  public String getCredDebIt() { 												//toString method
     return creditOrDebit + nl + nameFirst + " " + nameLast + nl + cardNum + nl + secCode + nl +   expDate + nl + seperator;
   }
 }
