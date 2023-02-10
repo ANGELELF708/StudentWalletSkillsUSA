f47bbd3d65423017368382c33c9b70fa7fd4fea3
ANGELELF708 <124015093+ANGELELF708@users.noreply.github.com>
2023-02-10 08:59:44 -0800

    Update Title.java

--- Title.java
+++ Title.java
@@ -1,31 +1,31 @@
-public class Title {
-  private String header;
-  private String subheader;
-  private String nl = System.getProperty("line.separator");
-  private String seperator = "---------------------------------------------";
+public class Title { 								//class for creating a title at the top of the screen
+  private String header; 							//string for storing a title header
+  private String subheader; 							//string for storing a title subheader
+  private String nl = System.getProperty("line.separator"); 			//creates a new line for formatting in the toString method
+  private String seperator = "---------------------------------------------"; 	//seperates lines for readability in the toString method
 //constructors
-  public Title(String header, String subheader) {
+  public Title(String header, String subheader) { 				//constructors
     this.header = header;
     this.subheader = subheader;
   }
 //methods
-  public String getHeader() {
+  public String getHeader() { 							//returns just the header
 	 return header;
   }
-  public void setHeader(String header) {
+  public void setHeader(String header) { 					//set the header to something else
 	 this.header = header;
   }
-  public String getSubheader() {
+  public String getSubheader() { 						//returns just the subheader
 	 return subheader;
   }
-  public void setSubheader(String subheader) {
+  public void setSubheader(String subheader) { 					//set the subheader to something else
 	 this.subheader = subheader;
   }
-  public void setTitle(String header, String subheader) {
+  public void setTitle(String header, String subheader) { 			//setst the entire title to something else
     this.header = header;
     this.subheader = subheader;
   }
-  public String getTitle() {
+  public String getTitle() { 							//toString method for printing the whole title
     return seperator + nl + header + nl + subheader + nl + seperator;
   }
 }
